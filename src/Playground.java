d8577f7a428ae940d45ac975ee84eb1bb5636b29
bernardperru <47976460+bernardperru@users.noreply.github.com>
2022-09-15 09:17:33 +0200

    new new

--- src/Playground.java
+++ src/Playground.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 public class Playground {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Automaton[] list = JSONParser.parse("samples/json/AG",true);
+        Automaton[] list = JSONParser.parse("samples/json/TestProject1",true);
 
         for (Automaton a: list) {
             SimpleTransitionSystem s = new SimpleTransitionSystem(a);
