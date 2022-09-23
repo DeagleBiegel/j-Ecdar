ac1e8f724ef6810565af1b67f33cb2f644e05227
DeagleBiegel <47975952+DeagleBiegel@users.noreply.github.com>
2022-09-23 10:12:50 +0200

    Removed IOException

--- src/Playground.java
+++ src/Playground.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class Playground {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try {
             Automaton[] automaton = JSONParser.parse("samples/json/cofmachine", true);
 
